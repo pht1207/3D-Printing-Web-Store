e7fe206f173e66057fcea3e92573cec4b6b6e16e
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-12 21:51:47 -0500

    added a gcode viewer to the client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import StlInsert from './components/StlInsert';
+import {GCodeViewer} from "react-gcode-viewer";
 
 
 function App() {
 //https://craftcloud3d.com/
+//https://www.npmjs.com/package/gcode
+
+const url = "https://storage.googleapis.com/ucloud-v3/6127a7f9aa32f718b8c1ab4f.gcode"
+const style = {
+  top: 0,
+  left: 0,
+  width: '100vw',
+  height: '100vh',
+}
+
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:"grey", width:"100vw", height:"100vh"}}>
       <h1>Print App</h1>
       <StlInsert/>
+      <GCodeViewer
+            orbitControls
+            showAxes
+            style={style}
+            url={url}
+        />
     </div>
   );
 }
