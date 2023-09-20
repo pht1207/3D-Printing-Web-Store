eac437ee38b69ed27c7c484de5e2bc144903cfc1
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-20 10:33:35 -0500

    Added slider functionality to show visiblity percentage of GCode layers. Also added some basic styling to make it easier to work on

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import './App.css';
+import React, { useState } from 'react';
 import StlInsert from './components/StlInsert';
-import GCodeViewerComponent from './components/GCodeViewer';
 import {GCodeViewer} from "react-gcode-viewer";
 
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [sliderValue, setSliderValue] = useState(100);
+  const [visiblePercentage, setVisiblePercentage] = useState(1)
+  
 //https://craftcloud3d.com/
-//https://www.npmjs.com/package/gcode
-
+//https://www.npmjs.com/package/react-gcode-viewer
 const url = "https://storage.googleapis.com/ucloud-v3/6127a7f9aa32f718b8c1ab4f.gcode"
-const style = {
-  top: 0,
-  left: 0,
-  width: '100vw',
-  height: '100vh',
+
+
+function GCodeLoadedFunction(){
+  setIsLoaded(true);
+  console.log('hamood');
 }
+
+const handleSliderChange = (event) => {
+  const newValue = parseInt(event.target.value, 10);
+  setSliderValue(newValue);
+  setVisiblePercentage(newValue/100)
+  console.log(sliderValue)
+};
+
+const slider = (
+  <input
+  type="range"
+  min={0}
+  max={100}
+  value={sliderValue}
+  onChange={handleSliderChange}
+/>
+)
+
+
 
 
   return (
-    <div className="App" style={{backgroundColor:"grey", width:"100vw", height:"100vh"}}>
+    <div className="App">
       <h1>Print App</h1>
       <StlInsert/>
-      <GCodeViewer
-            orbitControls
-            showAxes
-            style={style}
-            url={url}
-        />
+        <div className='GCodeViewerDiv'>
+          {isLoaded? slider : <p>asdjaslhdlajks</p>}
+          <GCodeViewer
+                orbitControls={true}
+                showAxes={true}
+                url={url}
+                style={{width:"100%", height:"100%"}}
+                floorProps={{gridLength:235, gridWidth:235}}
+                visible={visiblePercentage}
+                onFinishLoading={GCodeLoadedFunction}
+            />
+
+      </div>
     </div>
   );
 }
