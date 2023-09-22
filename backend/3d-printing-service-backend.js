b951472b493b721429cbcda504689ff5dd307b6d
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-21 23:07:01 -0500

    Made progress finding command to slice via cli, added comments regarding it

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -17,6 +17,15 @@ const directoryPath = "./gcodes/"
 app.post('/upload', upload.single('file'), async function (req, res, next) {
     console.log("filename: "+req.file.originalname)
     console.log(req.file.filename)
+    //changes the file extension of what was uploaded to a .stl
+    const currentPath = './uploads/'+req.file.filename;
+    const newPath = './uploads/'+req.file.filename+".stl";
+    fs.rename(currentPath, newPath, err =>{
+      if(err){
+        console.error("error converting file extension")
+      }
+      //else, continue program
+    })
 
     //const url = await parseSTL(req.file);
 
@@ -27,6 +36,7 @@ app.post('/upload', upload.single('file'), async function (req, res, next) {
 })
 
 //Hosts the gcode files to the users (used in the gcodeviewer component)
+//Accessed simply by doing http://localhost:5000/filename, that's it. It hosts the directory with all the gcodes with directoryPath
 app.use(express.static(directoryPath));
 
 
@@ -41,11 +51,14 @@ app.listen(port, () => {
 async function parseSTL(stlFile){
   return new Promise (resolve => {
 
+      //else, continue program
+    })
+
     //Write some kind of code to turn the stl into a gcode file
 
+
+    //After parsing, if it is complete, resolve the promise
     if(parsed === true){
       resolve();
     }
-    }
-  )
 }
