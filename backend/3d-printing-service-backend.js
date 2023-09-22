7049660dedcc6635745817aa417cae999e92ca23
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-21 22:07:43 -0500

    Removed unused code from backend node program

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -3,6 +3,8 @@ const multer  = require('multer')
 const app = express()
 const port = 5000;
 const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
+
 
 let cors = require('cors');
 app.use(cors());
@@ -16,27 +18,9 @@ app.get('/hi', (req, res) => {
 
 app.post('/upload', upload.single('file'), function (req, res, next) {
     console.log("filename: "+req.file.originalname)
-    res.setHeader('Content-Disposition', 'inline; filename="file.ext"'); // Change the filename as needed
-    res.setHeader('Content-Type', 'application/octet-stream'); // Change the content type as needed
-    res.send(data);
+
   // req.file is the `file` file
   // req.body will hold the text fields, if there were any
-})
-
-app.post('/uploadarray', upload.array('photos', 12), function (req, res, next) {
-  // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
-})
-
-const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-app.post('/cool-profile', cpUpload, function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-  //
-  // e.g.
-  //  req.files['avatar'][0] -> File
-  //  req.files['gallery'] -> Array
-  //
-  // req.body will contain the text fields, if there were any
 })
 
 app.listen(port, () => {
