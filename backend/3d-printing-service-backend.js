f221c4a6f6dcbca19c2f50a330580f22ef7918be
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-21 22:42:27 -0500

    Backend program now presents the client with the string assigned to the file by multer so it may be fetched from the frontend

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -11,18 +11,23 @@ const { parse } = require('path');
 app.use(cors());
 app.use(express.json());
 
+const directoryPath = "./gcodes/"
 
 
 app.post('/upload', upload.single('file'), async function (req, res, next) {
     console.log("filename: "+req.file.originalname)
     console.log(req.file.filename)
-    res.download("./uploads/"+req.file.filename);
-//    const url = await parseSTL(req.file);
+
+    //const url = await parseSTL(req.file);
+
+
+    //Sends filename to the host after parsing it so it can be displayed in their browser
+    res.send(req.file.filename);
     
-  //  res.send(url);
-  // req.file is the `file` file
-  // req.body will hold the text fields, if there were any
 })
+
+//Hosts the gcode files to the users (used in the gcodeviewer component)
+app.use(express.static(directoryPath));
 
 
 
@@ -35,12 +40,11 @@ app.listen(port, () => {
 //Parses the stl file into a gcode file
 async function parseSTL(stlFile){
   return new Promise (resolve => {
-    let name = stlFile.filename
 
     //Write some kind of code to turn the stl into a gcode file
 
     if(parsed === true){
-      resolve("uploads/"+name)
+      resolve();
     }
     }
   )
