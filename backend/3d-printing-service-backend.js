e8c58e4d6f6d4e192e3032f0ec2e85868a84a64a
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-21 22:27:56 -0500

    Added a parseSTL method, nothing is in it yet. Added methodology to send data to said method

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -7,22 +7,41 @@ const fs = require('fs');
 
 
 let cors = require('cors');
+const { parse } = require('path');
 app.use(cors());
 app.use(express.json());
 
-app.get('/hi', (req, res) => {
-  res.send('Hello World!')
-  console.log('hi executed')
-})
 
 
-app.post('/upload', upload.single('file'), function (req, res, next) {
+app.post('/upload', upload.single('file'), async function (req, res, next) {
     console.log("filename: "+req.file.originalname)
-
+    console.log(req.file.filename)
+    res.download("./uploads/"+req.file.filename);
+//    const url = await parseSTL(req.file);
+    
+  //  res.send(url);
   // req.file is the `file` file
   // req.body will hold the text fields, if there were any
 })
 
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+//Parses the stl file into a gcode file
+async function parseSTL(stlFile){
+  return new Promise (resolve => {
+    let name = stlFile.filename
+
+    //Write some kind of code to turn the stl into a gcode file
+
+    if(parsed === true){
+      resolve("uploads/"+name)
+    }
+    }
+  )
+}
