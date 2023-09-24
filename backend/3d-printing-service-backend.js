548ec53e66d5ee9612c72bfe5a59dc811d572ee1
Parker Throneberry <parkerthroneberry@gmail.com>
2023-09-24 10:42:12 -0500

    Changed port of backend to 5005 to accomodate my servers port already in use

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const multer  = require('multer')
 const app = express()
-const port = 5000;
+const port = 5005;
 const upload = multer({ dest: 'data/stl' })
 const fs = require('fs');
 
