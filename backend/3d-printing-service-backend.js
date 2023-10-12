77bf355eac6e998c8c3bf745633bb96f86927b30
pht1207 <parkerthroneberry@gmail.com>
2023-10-12 02:11:26 -0500

    Stripe webhooks now function properly

--- backend/3d-printing-service-backend.js
+++ backend/3d-printing-service-backend.js
@@ -2,19 +2,15 @@ const express = require('express')
 const multer  = require('multer')
 const app = express()
 const port = 5005;
-const upload = multer({ dest: 'data/stl' })
 const fs = require('fs');
 
 //Used to parse the body of post requests
-const bodyParser = require('body-parser');
-app.use(bodyParser.text());
 
 
 
 let cors = require('cors');
 const { parse, format } = require('path');
 app.use(cors());
-app.use(express.json());
 //Used in executing commands on the server
 
 
@@ -22,7 +18,108 @@ app.use(express.json());
 const stripe = require("stripe")('sk_test_51NpxjLJfFzW7oP7EXHvOrSGY9PBG6K7zlgWTc0msnf0ycJFBWJalNqmfBwgsSscRvsggUkx1ZwcaUJAOIlQUd8E900cCiribBn');
 
 
+//Everything below this comment is webhooks for data from Stripe regarding payments and charges
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_ufT4nc2StCxTUOcTk33cm9siu7eMNiwe";
 
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  console.log("WEBHOOK CALLED!")
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+  console.log(endpointSecret)
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    console.error(err.message)
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+  console.log(event)
+  console.log(event.type)
+
+
+
+
+  // Handle the event
+  switch (event.type) {
+    
+    case 'invoice.payment_succeeded':
+      console.log("invoice.payment_succeeded case found!")
+      const invoicePaymentSucceeded = event.data.object;
+      invoicePaymentSucceededFunction(invoicePaymentSucceeded);
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  
+  function invoicePaymentSucceededFunction(invoiceObject){
+    console.log("invoicePaymentSucceededFunction called!")
+    const metaData = invoiceObject.data.object.metadata;
+    const paymentIntentID = invoiceObject.data.object.payment_intent;
+    const orderIDs = metaData.split(" "); //Splits the metadata into many strings depending on space between them
+
+    //Make new directory with the paymentIntendID inside of './completedOrders'
+    fs.mkdir('./completedOrders/' + paymentIntentID, (err) => {
+      if (err) {
+        console.error("Error creating folder: ${err}");
+      }
+      else{
+        //Go through all the orders and put them into the completedOrders folder
+        for(let i = 0; i < orderIDs.length; i++){
+          const currentItemID = orderIDs[i]
+          
+            const currentSTLPath = './data/'+currentItemID+'/'+currentItemID+".stl";
+            const newSTLPath = './data/completedOrders'+paymentIntentID+'/'+currentItemID+".stl";     
+            //Moves the .stl and .gcode to the new folder
+
+            fs.rename(currentSTLPath, newSTLPath, err =>{
+              if(err){
+                console.error("error moving .stl file")
+              }
+              else{
+                const currentGCodePath = './data/'+currentItemID+'/'+currentItemID+".gcode";
+                const newGCodePath = './data/completedOrders'+paymentIntentID+'/'+currentItemID+".gcode";     
+    
+                fs.rename(currentGCodePath, newGCodePath, err =>{
+                  if(err){
+                    console.error("error moving .gcode file")
+                  }
+                  else{
+                    console.log(".stl and .gcode moved to completedOrders folder.")
+                  }
+                })
+              }
+            })
+        }
+        //Once loop is complete, create a text document that has some order info on it. Complete this once I know the above code actually works
+        //fs.mkfile
+      }
+    })
+  }
+
+
+         
+              
+
+
+
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+
+
+
+
+//Initializing this after the webhook
+const upload = multer({ dest: 'data/stl' })
+app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.text());
 
 
 
@@ -348,96 +445,3 @@ https
 
 
 
-
-//Everything below this comment is webhooks for data from Stripe regarding payments and charges
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = "whsec_b29e9d1ebc7acd11d829f7f2dac404dd60dc62c3bd16b9a5b2b48f1508cdecef";
-
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  console.log("WEBHOOK CALLED!")
-  const sig = request.headers['stripe-signature'];
-
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    console.log(event)
-    console.log(event.type)
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
-  console.log(event)
-  console.log(event.type)
-
-
-
-
-  // Handle the event
-  switch (event.type) {
-    
-    case 'invoice.payment_succeeded':
-      console.log("invoice.payment_succeeded case found!")
-      const invoicePaymentSucceeded = event.data.object;
-      invoicePaymentSucceededFunction(invoicePaymentSucceeded);
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-  
-  function invoicePaymentSucceededFunction(invoiceObject){
-    console.log("invoicePaymentSucceededFunction called!")
-    const metaData = invoiceObject.data.object.metadata;
-    const paymentIntentID = invoiceObject.data.object.payment_intent;
-    const orderIDs = metaData.split(" "); //Splits the metadata into many strings depending on space between them
-
-    //Make new directory with the paymentIntendID inside of './completedOrders'
-    fs.mkdir('./completedOrders/' + paymentIntentID, (err) => {
-      if (err) {
-        console.error("Error creating folder: ${err}");
-      }
-      else{
-        //Go through all the orders and put them into the completedOrders folder
-        for(let i = 0; i < orderIDs.length; i++){
-          const currentItemID = orderIDs[i]
-          
-            const currentSTLPath = './data/'+currentItemID+'/'+currentItemID+".stl";
-            const newSTLPath = './data/completedOrders'+paymentIntentID+'/'+currentItemID+".stl";     
-            //Moves the .stl and .gcode to the new folder
-
-            fs.rename(currentSTLPath, newSTLPath, err =>{
-              if(err){
-                console.error("error moving .stl file")
-              }
-              else{
-                const currentGCodePath = './data/'+currentItemID+'/'+currentItemID+".gcode";
-                const newGCodePath = './data/completedOrders'+paymentIntentID+'/'+currentItemID+".gcode";     
-    
-                fs.rename(currentGCodePath, newGCodePath, err =>{
-                  if(err){
-                    console.error("error moving .gcode file")
-                  }
-                  else{
-                    console.log(".stl and .gcode moved to completedOrders folder.")
-                  }
-                })
-              }
-            })
-        }
-        //Once loop is complete, create a text document that has some order info on it. Complete this once I know the above code actually works
-        //fs.mkfile
-      }
-    })
-  }
-
-
-         
-              
-
-
-
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
